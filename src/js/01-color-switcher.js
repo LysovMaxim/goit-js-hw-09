e191722e905e5a17aa1501341e48a7b99466f160
LysovMaxim <max_maran@icloud.com>
2023-03-18 12:02:41 +0200

    Пофіксив 1 з

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const bodyEl = document.querySelector('body');
 const startEl = document.querySelector('button[data-start]');
 const stopEl = document.querySelector('button[data-stop]');
-
+console.log(stopEl)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -15,12 +15,12 @@ function addStylesBody() {
   }, 1000);
   if (timerId) {
      startEl.disabled = true;
-    startEl.removeEventListener('click', addStylesBody);
+    // startEl.removeEventListener('click', addStylesBody);
   }
 }
 
 stopEl.addEventListener('click', () => {
   clearInterval(timerId);
-  startEl.addEventListener('click', addStylesBody);
+  // startEl.addEventListener('click', addStylesBody);
    startEl.disabled = false;
 });
