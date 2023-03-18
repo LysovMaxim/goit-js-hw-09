be947e51d3dc600ac16c8c54e2d2f214b409ff0c
LysovMaxim <max_maran@icloud.com>
2023-03-18 16:14:43 +0200

    Пофыксив 3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,44 @@
-
 function createPromise(position, delay) {
-  return new Promise ((resolve, reject)=> {
-      const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay })
       } else {
         reject({ position, delay })
       }
-  })
-}
+      
+    }, delay)
+    })
+  
+};
 const formEl = document.querySelector('form');
 console.log(formEl);
 
 let position = 0;
 
 formEl.addEventListener("submit", (event) => {
-  event.preventDefault()
+  event.preventDefault();
  
   let delay = Number(event.target.elements.delay.value);
   let step = Number(event.target.elements.step.value);
   let amount = event.target.elements.amount.value;
 
   
-  setTimeout(() => {
+  
     for (let i = 0; i < amount; i += 1) {
       position += 1;
       
-   
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        })
+      delay += step;
+    }
 
-   createPromise(position, delay)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    }) 
-       delay += step;
 }
-  },delay) 
-  }
-) 
+);
 
 
