26b6beaf23ac9211037f0cd026834b88c6f6dfb4
LysovMaxim <max_maran@icloud.com>
2023-03-18 09:49:55 +0200

    Пофыкств 3 та 2 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,7 @@ const bodyEl = document.querySelector('body');
 const startEl = document.querySelector('button[data-start]');
 const stopEl = document.querySelector('button[data-stop]');
 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,6 @@
 
 function createPromise(position, delay) {
-
   return new Promise ((resolve, reject)=> {
-   
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay })
