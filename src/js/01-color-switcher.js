a1ba5b43782ffcaab192c4beee98000b999ef36e
LysovMaxim <max_maran@icloud.com>
2023-03-18 11:52:07 +0200

    пофіксив 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,26 +2,25 @@ const bodyEl = document.querySelector('body');
 const startEl = document.querySelector('button[data-start]');
 const stopEl = document.querySelector('button[data-stop]');
 
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 startEl.addEventListener('click', addStylesBody);
-
+let timerId = null;
 
 function addStylesBody() {
-  const timerId = setInterval(() => {
+  timerId = setInterval(() => {
     bodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000);
   if (timerId) {
-     startEl.disabled = true;
-    // startEl.removeEventListener('click', addStylesBody);
+    //  startEl.disabled = true;
+    startEl.removeEventListener('click', addStylesBody);
   }
 }
 
 stopEl.addEventListener('click', () => {
   clearInterval(timerId);
-  // startEl.addEventListener('click', addStylesBody);
- startEl.disabled = false;
+  startEl.addEventListener('click', addStylesBody);
+  //  startEl.disabled = false;
 });
